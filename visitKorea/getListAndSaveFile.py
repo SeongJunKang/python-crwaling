3a11f2063ca94985698776656ad4ea1db2d06e40
kang seongjun <tjdwns8574@gmail.com>
2020-06-19 10:31:41 +0900

    fix : crawling site change as contentType

--- visitKorea/getListAndSaveFile.py
+++ visitKorea/getListAndSaveFile.py
@@ -6,12 +6,22 @@ json = requests.get(urlFormat.format(1,1)).json()
 
 body = json["body"]
 totalCount = body["totalCount"]
-f = open("list.txt",mode="at",encoding="utf-8");
+f = open("list.txt",mode="wt",encoding="utf-8");
+
+#contentType
+# 30000 : event
+# 11000 > : rem
+# 1000 < : ms
 
 totalPage = math.ceil(totalCount / 1000)
 for page in range(1,totalPage+1):
 	json = requests.get(urlFormat.format(page, 1000)).json()
 	body = json["body"]
 	result = body["result"]
+
 	for item in result:
-		f.write("{0}\n".format(item["cotId"]))
+		try:
+			f.write("{0},{1}\n".format(item["cotId"],item["contentType"]))
+		except:
+			print("{0} PASS".format(item["cotId"]))
+
--- visitKorea/main.py
+++ visitKorea/main.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-import os
 
 f = open('list.txt')
 lines = f.readlines()
@@ -8,14 +7,17 @@ driver = webdriver.Chrome(chromedriver)
 for line in lines:
 	line = line.rstrip("\n")
 	try:
-		fileExist = os.path.isfile("download/{0}.xml".format(line));
-		if fileExist == False :
-			print(line)
-			driver.get('https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid={0}'.format(line))
-			contents_list = driver.find_elements_by_css_selector('div#contents')
-			download = open("download/{0}.xml".format(line),mode="at",encoding="utf-8");
-			for contents in contents_list:
-				download.write(contents.get_attribute("innerHTML"))
+		param = line.split(',', maxsplit=2)
+		if int(param[1]) == 30000:
+			continue
+		elif int(param[1]) > 10000:
+			driver.get('https://korean.visitkorea.or.kr/detail/rem_detail.do?cotid={0}'.format(param[0]))
+		else:
+			driver.get('https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid={0}'.format(param[0]))
+		contents_list = driver.find_elements_by_css_selector('div#contents')
+		download = open("download/{0}.xml".format(param[0]),mode="wt",encoding="utf-8");
+		for contents in contents_list:
+			download.write(contents.get_attribute("innerHTML"))
 	except :
 		print('{0}에서 에러가 발생했습니다.'.format(line));
 print("VisitKorea Crawling Finished")
